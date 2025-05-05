9d80e8059cf1fb8ff007e165d626c262eade4f4c
nickcyborg <20133476+nickcyborg@users.noreply.github.com>
2025-05-05 17:11:52 -0400

    added error handling for ids without messages

--- routes/detailRouter.js
+++ routes/detailRouter.js
@@ -6,8 +6,11 @@ const messages = require('../data/messages.js')
 detailRouter.get('/:id', (req, res) => {
     const {id} = req.params
     const message = messages.filter((item) =>  {  
-        return id.toString() == item.id.toString()
+        return id.toString() === item.id.toString()
     })
+    if (!message[0]) {
+        return res.render("error")
+    }
     res.render("message", {message: message[0]})
 })
 
